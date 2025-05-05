f032196d12359396599180dce78f24974ebffb5d
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2025-05-05 09:18:03 -0300

    refactor: add header component

--- app/(dashboard)/products/page.tsx
+++ app/(dashboard)/products/page.tsx
@@ -2,25 +2,28 @@ import { DataTable } from "@/app/_components/ui/data-table";
 import { productsTableColumns } from "./_components/table-columns";
 import { getProducts } from "@/app/_data-access/products/get-products";
 import CreateProductButton from "./_components/create-product-button";
-
-// Essa página será montada uma vez e reutilizada (SSG), podendo ser incrementada de forma regenerativa (ISR)
-export const dynamic = "force-static";
+import Header, {
+  HeaderLeft,
+  HeaderRight,
+  HeaderSubtitle,
+  HeaderTitle,
+} from "@/app/_components/header";
 
 const Productspage = async () => {
   const products = await getProducts();
 
   return (
     <div className="m-8 w-full space-y-8 rounded-lg bg-white p-8">
-      <div className="flex w-full items-center justify-between">
-        <div className="space-y-1">
-          <span className="text-xs font-semibold text-slate-500">
-            Gestão de Produtos
-          </span>
-          <h1 className="text-xl font-semibold">Produtos</h1>
-        </div>
+      <Header>
+        <HeaderLeft>
+          <HeaderSubtitle>Gestão de Produtos</HeaderSubtitle>
+          <HeaderTitle>Produtos</HeaderTitle>
+        </HeaderLeft>
 
-        <CreateProductButton />
-      </div>
+        <HeaderRight>
+          <CreateProductButton />
+        </HeaderRight>
+      </Header>
 
       <DataTable
         columns={productsTableColumns}
--- app/(dashboard)/sales/page.tsx
+++ app/(dashboard)/sales/page.tsx
@@ -4,6 +4,12 @@ import CreateSaleButton from "./_components/create-sale-button";
 import { DataTable } from "@/app/_components/ui/data-table";
 import { saleTableColumns } from "./_components/table-columns";
 import { getSales } from "@/app/_data-access/sale/get-sales";
+import Header, {
+  HeaderLeft,
+  HeaderRight,
+  HeaderSubtitle,
+  HeaderTitle,
+} from "@/app/_components/header";
 
 const SalesPage = async () => {
   const products = await getProducts();
@@ -22,16 +28,19 @@ const SalesPage = async () => {
 
   return (
     <div className="m-8 w-full space-y-8 rounded-lg bg-white p-8">
-      <div className="flex w-full items-center justify-between">
-        <div className="space-y-1">
-          <span className="text-xs font-semibold text-slate-500">
-            Gestão de Vendas
-          </span>
-          <h1 className="text-xl font-semibold">Vendas</h1>
-        </div>
+      <Header>
+        <HeaderLeft>
+          <HeaderSubtitle>Gestão de Vendas</HeaderSubtitle>
+          <HeaderTitle>Vendas</HeaderTitle>
+        </HeaderLeft>
 
-        <CreateSaleButton products={products} productOptions={productOptions} />
-      </div>
+        <HeaderRight>
+          <CreateSaleButton
+            products={products}
+            productOptions={productOptions}
+          />
+        </HeaderRight>
+      </Header>
 
       <DataTable columns={saleTableColumns} data={tableData} />
     </div>
