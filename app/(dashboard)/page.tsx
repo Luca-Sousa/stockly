321673540d99f8383e373fb3792a8294c5c8fab3
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2025-05-06 12:02:17 -0300

    feat: add most sold products card component with streaming

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -4,8 +4,6 @@ import Header, {
   HeaderTitle,
 } from "../_components/header";
 import { SummaryCardSkeleton } from "./_components/summary-card";
-import { getDashboard } from "../_data-access/dashboard/get-dashboard";
-import MostSoldProductItem from "./_components/most-sold-product-item";
 import { Suspense } from "react";
 import TotalRevenueCard from "./_components/total-revenue-card";
 import TodayRevenueCard from "./_components/today-revenue-card";
@@ -14,10 +12,11 @@ import TotalInStockCard from "./_components/total-in-stock-card";
 import TotalProductsCard from "./_components/total-products-card";
 import TotalLast14DaysRevenueChart from "./_components/total-last-14-days-revenue-card";
 import { Skeleton } from "../_components/ui/skeleton";
+import MostSoldProductCard, {
+  MostSoldProductsSkeleton,
+} from "./_components/most-sold-products-card";
 
 const Home = async () => {
-  const { mostSoldProducts } = await getDashboard();
-
   return (
     <div className="m-8 flex w-full flex-col space-y-8 rounded-lg">
       <Header>
@@ -65,17 +64,9 @@ const Home = async () => {
           <TotalLast14DaysRevenueChart />
         </Suspense>
 
-        <div className="flex h-full flex-col overflow-hidden rounded-xl bg-white">
-          <p className="p-6 text-lg font-semibold text-slate-900">
-            Produtos Mais Vendidos
-          </p>
-
-          <div className="space-y-7 overflow-y-auto px-6 pb-6">
-            {mostSoldProducts.map((product) => (
-              <MostSoldProductItem key={product.productId} product={product} />
-            ))}
-          </div>
-        </div>
+        <Suspense fallback={<MostSoldProductsSkeleton />}>
+          <MostSoldProductCard />
+        </Suspense>
       </div>
     </div>
   );
